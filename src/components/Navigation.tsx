8de269e0742c6cf464e593614330d0cf15e8ce12
Jason <voychishin.jason@gmail.com>
2022-10-18 11:11:43 +0300

    updated routes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ export function Navigation() {
         <nav className="h-[50px]  flex justify-between items-center px-5 bg-gray-500 text-white">
             <span className="font-bold">React 2022</span>
             <span>
-                <Link to="/" className='mr-2'>Products</Link>
+                <Link to="/react-ts" className='mr-2'>Products</Link>
                 <Link to="/about">About</Link>
             </span>
         </nav>
